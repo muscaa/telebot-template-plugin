f3593af33493a70ba68084a0cb0622abe18658dc
Musca Mihail <muscamihaill@gmail.com>
2025-05-09 01:06:24 +0300

    fixed zip name

--- scripts/utils/project.py
+++ scripts/utils/project.py
@@ -7,4 +7,4 @@ def get_triplet() -> str:
     return f"{p.ARCH}-{p.SYSTEM}-{CONFIGURATION}"
 
 def get_package_zip_name() -> str:
-    return f"{config.NAME}-{p.SYSTEM}-{p.ARCH}.zip"
+    return f"{config.NAME}.zip"
