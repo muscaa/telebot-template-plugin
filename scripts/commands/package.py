ba21712e0fab6746bb7172d089d1b7e593d310c8
Musca Mihail <muscamihaill@gmail.com>
2025-05-09 21:38:52 +0300

    improved package command, remove package libraries

--- scripts/commands/package.py
+++ scripts/commands/package.py
@@ -1,4 +1,5 @@
 import zipfile
+import os
 
 import config
 from utils import platform as p
@@ -7,12 +8,31 @@ from utils import project
 def package():
     print(f"Packaging {project.get_package_zip_name()}...")
 
+    added_files = {}
+    build_dir = f"build/{project.get_triplet()}/{project.CONFIGURATION}"
     with zipfile.ZipFile(f"build/{project.get_package_zip_name()}", "w", zipfile.ZIP_DEFLATED) as zip:
         for lib in config.PACKAGE_LIBRARIES:
             if lib.is_valid():
-                zip.write(f"build/{project.get_triplet()}/{project.CONFIGURATION}/{lib.get_path()}", lib.get_path_to())
+                src = os.path.join(build_dir, lib.get_path())
+                dst = lib.get_path_to()
+                zip.write(src, dst)
+                added_files[src] = dst
 
         for file in config.PACKAGE_OTHER:
             if file.is_valid():
-                zip.write(file.get_path(), file.get_path_to())
+                src = file.get_path()
+                dst = file.get_path_to()
+                zip.write(src, dst)
+                added_files[src] = dst
+
+        for root, _, files in os.walk(build_dir):
+            for file in files:
+                src = os.path.join(root, file)
+                if src not in added_files:
+                    dst = f"{p.SYSTEM}-{p.ARCH}.{os.path.relpath(src, build_dir)}"
+                    zip.write(src, dst)
+                    added_files[src] = dst
+
+    for src, dst in added_files.items():
+        print(f"{src} -> {dst}")
     
--- scripts/config.py
+++ scripts/config.py
@@ -4,8 +4,6 @@ from utils import platform as p
 NAME: str = "telebot-template-plugin"
 
 PACKAGE_LIBRARIES: list[Lib] = [
-    Lib(f"lib{NAME}", path_to=f"plugin-{p.SYSTEM}-{p.ARCH}", platforms=[p.LINUX, p.MACOS]),
-    Lib(NAME, path_to=f"plugin-{p.SYSTEM}-{p.ARCH}", platforms=[p.WINDOWS]),
 ]
 PACKAGE_OTHER: list[File] = [
     File("plugin.json"),
