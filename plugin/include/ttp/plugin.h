1304151aaa97b72b430aa0d4756624b97be8d698
Musca Mihail <muscamihaill@gmail.com>
2025-05-14 18:04:39 +0300

    minor changes

---
+++ plugin/include/ttp/plugin.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <boost/config.hpp>
+#include <telebot/utils/logging.h>
+
+namespace ttp {
+
+BOOST_SYMBOL_EXPORT void main();
+
+} // namespace ttp
---
+++ plugin/src/ttp/plugin.cpp
@@ -0,0 +1,11 @@
+#include "ttp/plugin.h"
+
+namespace ttp {
+
+namespace log = telebot::utils::logging;
+
+void main() {
+    log::info("Hello from telebot template plugin!");
+}
+
+} // namespace ttp
